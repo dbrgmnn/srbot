5b5bfdd9ed023a6e49531eb723dbe2215fab5565
Dmitrii Bergmann <dmtrbrgmnn@gmail.com>
2026-03-09 01:58:02 +0100

    Enhance versioning update in update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/pi/srbot
 git pull origin main
-sed -i "s/?v=[0-9]*/?v=$(date +%s)/g" webapp/index.html
+sed -i "s/\.css?v=[0-9]*/.css?v=$(date +%s)/g;s/\.js?v=[0-9]*/.js?v=$(date +%s)/g" webapp/index.html
 source venv/bin/activate
 pip install -r requirements.txt
 sudo systemctl restart srbot
