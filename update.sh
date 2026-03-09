33d18c63b9a91c3a6cb6c19b3adacd942dd6dceb
Dmitrii Bergmann <dmtrbrgmnn@gmail.com>
2026-03-09 01:47:10 +0100

    front: added versions to index

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/pi/srbot
 git pull origin main
+sed -i "s/?v=[0-9]*/?v=$(date +%s)/g" webapp/index.html
 source venv/bin/activate
 pip install -r requirements.txt
 sudo systemctl restart srbot
