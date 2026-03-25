245d2e1e0bd9139d0e0f1daa63014531561555d7
Dmitrii Bergmann <dmtrbrgmnn@gmail.com>
2026-03-26 00:06:47 +0100

    fix: resolve circular dependency in app_keys

--- api/app_keys.py
+++ api/app_keys.py
@@ -3,12 +3,11 @@ from collections import OrderedDict
 from aiohttp import web
 
 from config import Config
-from core.scheduler import Scheduler
 
 # Define Typed Keys for Aiohttp App
 CONFIG_KEY = web.AppKey("config", Config)
 DB_KEY = web.AppKey("db", object)  # aiosqlite.Connection
-SCHEDULER_KEY = web.AppKey("scheduler", Scheduler)
+SCHEDULER_KEY = web.AppKey("scheduler", object)
 USER_CACHE_KEY = web.AppKey("user_cache", OrderedDict)
 HTTP_SESSION_KEY = web.AppKey("http_session", object)
 TRANSLATOR_KEY = web.AppKey("translator", object)
