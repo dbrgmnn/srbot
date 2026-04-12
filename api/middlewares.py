e8c6ad9d9a295832627bff7b51171ba029da97ba
Dmitrii Bergmann <dmtrbrgmnn@gmail.com>
2026-04-12 16:49:07 +0200

    refactor: repository_middleware

--- api/middlewares.py
+++ api/middlewares.py
@@ -1,14 +1,13 @@
+from aiohttp import web
+
 from api.app_keys import DB_KEY
 from db import UserRepo, WordRepo
 
 
-def repository_middleware(handler):
+@web.middleware
+async def repository_middleware(request, handler):
     """Middleware to inject repositories into the request."""
-
-    async def middleware(request, handler):
-        db = request.app[DB_KEY]
-        request["user_repo"] = UserRepo(db)
-        request["word_repo"] = WordRepo(db)
-        return await handler(request)
-
-    return middleware
+    db = request.app[DB_KEY]
+    request["user_repo"] = UserRepo(db)
+    request["word_repo"] = WordRepo(db)
+    return await handler(request)
